2fe73552378f9c2d7a676f6d7449c289c07cdca9
EmmaJelena <emmajelena@icloud.com>
2024-02-17 17:29:40 -0700

    The cat and mouse flip

---
+++ MouseSprite.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Set the initial scale of the sprite
+	scale = Vector2(0.5, 0.5)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Check for arrow key input
+	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
+	# Flip the sprite horizontally based on input
+	if horizontal_input != 0:
+		# Set the scale.x property to -1 to flip horizontally
+		scale.x = abs(scale.x) * horizontal_input
