b179a8e8c5653de052d2bb4e7a0d9f287e97fc41
EmmaJelena <emmajelena@icloud.com>
2024-02-18 11:28:00 -0700

    Musk is Mortal

--- ElonMusk.gd
+++ ElonMusk.gd
@@ -40,3 +40,9 @@ func _on_kill_the_cat_body_entered(body):
 	if body.name == "theCat":
 		body.health -= 5
 		print(body.health)
+
+
+
+func _on_death_by_cat_body_entered(body):
+	if body.name == "theCat":
+		queue_free()
